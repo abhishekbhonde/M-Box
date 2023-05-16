dec98a662fe27d8c4d4e4b419021b1d38c2cf1b6
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-05-16 22:14:16 +0530

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -41,22 +41,78 @@ class MyApp extends StatelessWidget {
 
                 ],
               ),
-              SizedBox(height: 40,),
+              SizedBox(height: 30,),
               Center(
                 child: Text("Login",
                 style: TextStyle(fontFamily: 'Rubik Medium', fontSize: 24, color: Color(0xff2D3142),
                 ),
                 ),
               ),
-              SizedBox(height: 14,),
+              SizedBox(height: 20,),
               Center(
                 child: Text("This is Abhishek from 2nd \n year's  Comp Department",
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontFamily: 'Rubik Regular', fontSize: 24, color: Color(0xff4C5980),
+                  style: TextStyle(fontFamily: 'Rubik Regular', fontSize: 20, color: Color(0xff4C5980),
                   ),
                 ),
               ),
-              SizedBox(height: 400,),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    fillColor: const Color(0xffF9F9FA),
+                    filled: true,
+                    prefixIcon: const Icon(Icons.alternate_email, color: Colors.black),
+
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    fillColor: const Color(0xffF9F9FA),
+                    filled: true,
+                    prefixIcon: const Icon(Icons.lock, color: Colors.black),
+                    suffixIcon: Icon(Icons.visibility),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xffE4E7EB)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: EdgeInsets.only(left: 220),
+                child: Center(
+                  child: Text('Forgot Password',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    decoration: TextDecoration.underline,
+                  ),
+                  ),
+
+                ),
+              ),
+              SizedBox(height: 220,),
               Container(
 
                 height: 50,
